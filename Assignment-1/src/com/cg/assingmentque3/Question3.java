7f18a5f9cf7fb311d8c1b0d936bf5c9b3ae20923
RASHMI <RASHMI@DESKTOP-OA8380P>
2022-08-10 12:45:12 +0530

    second Commit

--- Assignment-1/src/com/cg/assingmentque3/Question3.java
+++ Assignment-1/src/com/cg/assingmentque3/Question3.java
@@ -7,18 +7,16 @@ public class Question3
     public static void pyramidPattern(int n) 
     {  
     	   int i,j;
-        for ( i=0; i<n; i++) //outer loop for number of rows(n) { for (int j=n-i; j>1; j--) //inner loop for spaces
+        for ( i=0; i<n; i++)
+        { 
+                System.out.print("\n"); 
+            for (j=0; j<=i; j++ ) 
             { 
-                System.out.print(" "); //print space
-            }  
-            for (j=0; j<=i; j++ ) //inner loop for number of columns
-            { 
-                System.out.print("* "); //print star
+                System.out.print("*");
             } 
   
-            System.out.println(); //ending line after each row
         } 
-
+    }
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n = s.nextInt();
